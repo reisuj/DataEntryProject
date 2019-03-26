8283893dec901687bce92b4803b0c5e368e705a1
Branch <BBRANCH@broward.org>
2019-03-26 15:32:52 -0400

    Add project files.

---
+++ DataEntryProject/Form1.Designer.cs
@@ -0,0 +1,222 @@
+﻿namespace DataEntryProject
+{
+    partial class frmDataEntry
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.grbDataEntry = new System.Windows.Forms.GroupBox();
+            this.lblZip = new System.Windows.Forms.Label();
+            this.lblState = new System.Windows.Forms.Label();
+            this.lblCity = new System.Windows.Forms.Label();
+            this.lblAddress = new System.Windows.Forms.Label();
+            this.lblName = new System.Windows.Forms.Label();
+            this.grbTimer = new System.Windows.Forms.GroupBox();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.txtAddress = new System.Windows.Forms.TextBox();
+            this.txtCity = new System.Windows.Forms.TextBox();
+            this.txtState = new System.Windows.Forms.TextBox();
+            this.txtZip = new System.Windows.Forms.TextBox();
+            this.btnAccept = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.grbDataEntry.SuspendLayout();
+            this.grbTimer.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // grbDataEntry
+            // 
+            this.grbDataEntry.Controls.Add(this.btnClear);
+            this.grbDataEntry.Controls.Add(this.btnAccept);
+            this.grbDataEntry.Controls.Add(this.txtZip);
+            this.grbDataEntry.Controls.Add(this.txtState);
+            this.grbDataEntry.Controls.Add(this.txtCity);
+            this.grbDataEntry.Controls.Add(this.txtAddress);
+            this.grbDataEntry.Controls.Add(this.txtName);
+            this.grbDataEntry.Controls.Add(this.lblZip);
+            this.grbDataEntry.Controls.Add(this.lblState);
+            this.grbDataEntry.Controls.Add(this.lblCity);
+            this.grbDataEntry.Controls.Add(this.lblAddress);
+            this.grbDataEntry.Controls.Add(this.lblName);
+            this.grbDataEntry.Location = new System.Drawing.Point(50, 45);
+            this.grbDataEntry.Name = "grbDataEntry";
+            this.grbDataEntry.Size = new System.Drawing.Size(285, 344);
+            this.grbDataEntry.TabIndex = 0;
+            this.grbDataEntry.TabStop = false;
+            this.grbDataEntry.Text = "Data Entry";
+            // 
+            // lblZip
+            // 
+            this.lblZip.AutoSize = true;
+            this.lblZip.Location = new System.Drawing.Point(31, 226);
+            this.lblZip.Name = "lblZip";
+            this.lblZip.Size = new System.Drawing.Size(22, 13);
+            this.lblZip.TabIndex = 4;
+            this.lblZip.Text = "Zip";
+            // 
+            // lblState
+            // 
+            this.lblState.AutoSize = true;
+            this.lblState.Location = new System.Drawing.Point(31, 176);
+            this.lblState.Name = "lblState";
+            this.lblState.Size = new System.Drawing.Size(32, 13);
+            this.lblState.TabIndex = 3;
+            this.lblState.Text = "State";
+            // 
+            // lblCity
+            // 
+            this.lblCity.AutoSize = true;
+            this.lblCity.Location = new System.Drawing.Point(31, 128);
+            this.lblCity.Name = "lblCity";
+            this.lblCity.Size = new System.Drawing.Size(24, 13);
+            this.lblCity.TabIndex = 2;
+            this.lblCity.Text = "City";
+            // 
+            // lblAddress
+            // 
+            this.lblAddress.AutoSize = true;
+            this.lblAddress.Location = new System.Drawing.Point(31, 87);
+            this.lblAddress.Name = "lblAddress";
+            this.lblAddress.Size = new System.Drawing.Size(45, 13);
+            this.lblAddress.TabIndex = 1;
+            this.lblAddress.Text = "Address";
+            // 
+            // lblName
+            // 
+            this.lblName.AutoSize = true;
+            this.lblName.Location = new System.Drawing.Point(31, 37);
+            this.lblName.Name = "lblName";
+            this.lblName.Size = new System.Drawing.Size(35, 13);
+            this.lblName.TabIndex = 0;
+            this.lblName.Text = "Name";
+            // 
+            // grbTimer
+            // 
+            this.grbTimer.Controls.Add(this.textBox1);
+            this.grbTimer.Location = new System.Drawing.Point(454, 45);
+            this.grbTimer.Name = "grbTimer";
+            this.grbTimer.Size = new System.Drawing.Size(200, 344);
+            this.grbTimer.TabIndex = 1;
+            this.grbTimer.TabStop = false;
+            this.grbTimer.Text = "Timer";
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(113, 30);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(129, 20);
+            this.txtName.TabIndex = 5;
+            // 
+            // txtAddress
+            // 
+            this.txtAddress.Location = new System.Drawing.Point(113, 80);
+            this.txtAddress.Name = "txtAddress";
+            this.txtAddress.Size = new System.Drawing.Size(129, 20);
+            this.txtAddress.TabIndex = 6;
+            // 
+            // txtCity
+            // 
+            this.txtCity.Location = new System.Drawing.Point(113, 121);
+            this.txtCity.Name = "txtCity";
+            this.txtCity.Size = new System.Drawing.Size(129, 20);
+            this.txtCity.TabIndex = 7;
+            // 
+            // txtState
+            // 
+            this.txtState.Location = new System.Drawing.Point(113, 169);
+            this.txtState.Name = "txtState";
+            this.txtState.Size = new System.Drawing.Size(129, 20);
+            this.txtState.TabIndex = 8;
+            // 
+            // txtZip
+            // 
+            this.txtZip.Location = new System.Drawing.Point(113, 219);
+            this.txtZip.Name = "txtZip";
+            this.txtZip.Size = new System.Drawing.Size(129, 20);
+            this.txtZip.TabIndex = 9;
+            // 
+            // btnAccept
+            // 
+            this.btnAccept.Location = new System.Drawing.Point(34, 273);
+            this.btnAccept.Name = "btnAccept";
+            this.btnAccept.Size = new System.Drawing.Size(75, 23);
+            this.btnAccept.TabIndex = 10;
+            this.btnAccept.Text = "Accept";
+            this.btnAccept.UseVisualStyleBackColor = true;
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(156, 272);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(75, 23);
+            this.btnClear.TabIndex = 11;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(56, 37);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 0;
+            // 
+            // frmDataEntry
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.grbTimer);
+            this.Controls.Add(this.grbDataEntry);
+            this.Name = "frmDataEntry";
+            this.Text = "Data Entry Form";
+            this.grbDataEntry.ResumeLayout(false);
+            this.grbDataEntry.PerformLayout();
+            this.grbTimer.ResumeLayout(false);
+            this.grbTimer.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox grbDataEntry;
+        private System.Windows.Forms.Label lblZip;
+        private System.Windows.Forms.Label lblState;
+        private System.Windows.Forms.Label lblCity;
+        private System.Windows.Forms.Label lblAddress;
+        private System.Windows.Forms.Label lblName;
+        private System.Windows.Forms.GroupBox grbTimer;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Button btnAccept;
+        private System.Windows.Forms.TextBox txtZip;
+        private System.Windows.Forms.TextBox txtState;
+        private System.Windows.Forms.TextBox txtCity;
+        private System.Windows.Forms.TextBox txtAddress;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
